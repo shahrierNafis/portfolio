b34b778c6ed8939113a5f3c9e9909a1e5d4d6ba8
shahrierNafis <102902752+shahrierNafis@users.noreply.github.com>
2024-01-09 07:08:19 +0600

    Refactor: ParticleEffect to use tsparticles

--- src/components/ParticleEffect.tsx
+++ src/components/ParticleEffect.tsx
@@ -1,20 +1,95 @@
-import { Particle } from "jparticles";
 import { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
 function ParticleEffect() {
+  const [init, setInit] = useState(false);
+  const [number, setNumber] = useState(0);
+  const [distance, setDistance] = useState(0);
+  const [radius, setRadius] = useState(0);
   useEffect(() => {
-    new Particle("#particle", {
-      color: "white",
-      proximity: 1000,
-      parallax: true,
-      range: innerWidth < 1024 ? 50 : 100,
-      num: innerWidth / 10,
-      maxSpeed: 0,
-      lineShape: "cube",
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
     });
   }, []);
 
-  return <div id="particle" className="fixed w-screen h-screen"></div>;
-}
+  useEffect(() => {
+    setNumber(innerWidth / 10);
+    setDistance(Math.max(innerHeight, innerWidth));
+    setRadius(innerWidth < 768 ? innerHeight * 0.25 : 150);
+  }, []);
 
+  return (
+    <>
+      {init && (
+        <Particles
+          id="tsparticles"
+          options={{
+            background: {
+              color: {
+                value: "black",
+              },
+            },
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onHover: {
+                  enable: true,
+                  mode: "connect",
+
+                  parallax: {
+                    enable: true,
+                    force: 5,
+                    smooth: 25,
+                  },
+                },
+              },
+              modes: {
+                connect: {
+                  distance,
+                  radius,
+                  links: {
+                    opacity: 0.1,
+                  },
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              collisions: { enable: true },
+              move: {
+                size: true,
+                direction: "none",
+                enable: true,
+                outModes: {
+                  default: "out",
+                },
+                random: true,
+                speed: 0.25,
+                straight: false,
+              },
+              number: {
+                value: number,
+              },
+              opacity: {
+                value: 1,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 1, max: 2.5 },
+              },
+            },
+            detectRetina: true,
+          }}
+        />
+      )}
+    </>
+  );
+}
 export default ParticleEffect;
